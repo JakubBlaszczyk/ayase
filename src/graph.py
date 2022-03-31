ca4f9cf4b3833a6706d12b748d34f392cc741272
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2022-03-31 15:25:03 +0200

    Initial commit

---
+++ src/graph.py
@@ -0,0 +1,62 @@
+from array import array
+from ctypes import sizeof
+from random import Random
+import re
+import string
+from typing import List
+
+
+class Vertex:
+  def __init__(self) -> None:
+    self.neighbours = array('i')
+    self.color: int
+    self.pff: int
+
+  def add_neighbour(self, neighbour: int) -> None:
+    self.neighbours.append(neighbour)
+
+  def get_neighbours_list(self) -> List:
+    return self.neighbours
+
+
+class Graph:
+  def __init__(self) -> None:
+    self.graph: List[Vertex] = []
+
+  def load_from_file(self, path: string) -> None:
+    self.graph.clear()
+    with open(path, mode='r') as f:
+      for line in f.readlines():
+        if line.split()[0] == 'p':
+          arguments: List[str] = re.split(r' |\n', line)
+          original_size: int = arguments[2]
+          for i in range(0, (original_size * 2)):
+            self.graph.append(Vertex())
+          # print("length", len(graph))
+        if line.split()[0] == 'e':
+          arguments: List[str] = re.split(r' |\n', line)
+          if arguments[1] != arguments[2]:
+            # print("argument1", int(arguments[1]), "argument2", int(arguments[2]))
+            self.graph[int(arguments[1]) -
+                       1].add_neighbour(neighbour=int(arguments[2]))
+            self.graph[int(
+                arguments[1]) - 1].add_neighbour(neighbour=int(arguments[1] - 1 + original_size))
+            self.graph[int(arguments[1] - 1 + original_size)
+                       ].add_neighbour(neighbour=int(arguments[1]))
+    # print("Vertex %i" % 0)
+    # print("Neighbours", graph[0].get_neighbours_list(), "address", id(graph[0]))
+
+  def _colorize_graph(self, color_amount: int) -> None:
+    for element in self.graph:
+      element.color = Random.randint(0, color_amount)
+
+  def _evaluate_pff(self) -> None:
+    for element in self.graph:
+      for neighbour in element.neighbours:
+        if element.color == self.graph[neighbour].color:
+          element.pff = element.pff + 1
+
+  def _evaluate_conflicts(self) -> None:
+    count: int = 0
+    for element in self.graph:
+      count = count + element.pff
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from graph import read_from_file
+
+if __name__ == '__main__':
+  read_from_file("graphs/queen8_8g.col")
